3238b6ab07903078ab9167b0c6a40a8a17a7e84e
Jan <jan.cimbalnik@fnusa.cz>
2024-08-19 10:56:11 +0200

    Add custom command to parse out flag constants from medlib.h.

--- setup.py
+++ setup.py
@@ -23,9 +23,45 @@
 # !/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-
+import json
 from setuptools import Extension, setup
 import numpy
+
+from setuptools.command.install import install
+
+class CustomInstallCommand(install):
+    def run(self):
+
+        with open('dhn_medlib/medlib_m12.h') as f:
+            lines = f.readlines()
+
+        flag_dict = {
+            'LH': {},
+            'DM': {}
+        }
+
+        for line in lines:
+            if line.startswith('#define'):
+                mod_line = line.replace('#define ', '').replace('\n', '')
+                mod_line = mod_line.lstrip()
+
+                # Remove commens
+                mod_line = mod_line.split('//')[0]
+
+                for prefix in ['LH', 'DM']:
+                    if mod_line.startswith(prefix) and 'ui8' in mod_line and '<<' in mod_line:
+                        def_val = [x for x in mod_line.split('\t') if len(x)]
+                        val_str = def_val[1]
+                        pos_str = val_str.split('<<')[1]
+                        pos = int(pos_str.replace(')', '').strip())
+
+                        flag_dict[prefix][def_val[0]] = pos
+
+        json_string = json.dumps(flag_dict)
+
+        with open("src/dhn_med_py/medlib_flags.py", "w") as outfile:
+            outfile.write(f'FLAGS = {json_string}')
+        install.run(self)
 
 FILE_EXT = Extension("dhn_med_py.med_file.dhnmed_file",
              ["src/dhn_med_py/med_file/dhnmed_file.c"],
@@ -35,5 +71,6 @@ FILE_EXT = Extension("dhn_med_py.med_file.dhnmed_file",
 setup(name="dhn_med_py",
       zip_safe=False,
       package_dir={"": "src"},
+      cmdclass={'install': CustomInstallCommand},
       ext_modules=[FILE_EXT],
       include_dirs=[numpy.get_include()])
